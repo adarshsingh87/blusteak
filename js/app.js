3d28cb52060a19464cad569bb5c6fdec6d7f4f45
Adarsh Singh <adarshmanoj.05.as@gmail.com>
2020-11-19 23:47:46 +0530

    initial commit

---
+++ js/app.js
@@ -0,0 +1,139 @@
+// ! navbar
+const navSlide = () => {
+	const burger = document.querySelector('.burger')
+	const nav = document.querySelector('.nav-links')
+	const navLinks = document.querySelectorAll('.nav-links li')
+	const navLinksAncor = document.querySelectorAll('.nav-links li a')
+	// Toggle nav
+	burger.addEventListener('click', () => {
+		nav.classList.toggle('nav-active')
+		//animate links
+		navLinks.forEach((link, index) => {
+			if (link.style.animation) {
+				link.style.animation = ''
+			} else {
+				link.style.animation = `navLinkFade 0.5s ease forwards ${
+					index / 5 + 0.3
+				}s`
+			}
+		})
+		//burger animation
+		burger.classList.toggle('toggle')
+	})
+	navLinksAncor.forEach((link) => {
+		link.addEventListener('click', () => {
+			nav.classList.remove('nav-active')
+			navLinks.forEach((link, index) => {
+				if (link.style.animation) {
+					link.style.animation = ''
+				}
+			})
+			burger.classList.remove('toggle')
+		})
+	})
+}
+
+navSlide()
+
+jQuery(function ($) {
+	var $nav = $('.container-nav')
+	var $win = $(window)
+	var winH = $win.height() // Get the window height.
+
+	$win
+		.on('scroll', function () {
+			if ($(this).scrollTop() > winH*0.9) {
+				$nav.addClass('doch')
+			} else {
+				$nav.removeClass('doch')
+			}
+		})
+		.on('resize', function () {
+			// If the user resizes the window
+			winH = $(this).height() // you'll need the new height value
+		})
+} )
+
+// ! carousel
+$('.pri-carousel').owlCarousel({
+    loop:false,
+  stagePadding: 15,
+    margin:10,
+    nav:true,
+  navText : ['<span class="uk-margin-small-right uk-icon" uk-icon="icon: chevron-left"></span>','<span class="uk-margin-small-left uk-icon" uk-icon="icon: chevron-right"></span>'],
+  loop: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        640:{
+            items:2
+        },
+      960:{
+            items:3
+        }
+    }
+})
+
+$('.sec-carousel').owlCarousel({
+    loop:false,
+  stagePadding: 15,
+    margin:10,
+    nav:true,
+  navText : ['<span class="uk-margin-small-right uk-icon" uk-icon="icon: chevron-left"></span>','<span class="uk-margin-small-left uk-icon" uk-icon="icon: chevron-right"></span>'],
+  loop: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        640:{
+            items:1
+        },
+      960:{
+            items:1
+        }
+    }
+})
+
+// ! faq accordian
+// CSS animation maps:
+const rotateH = {
+  transform: 'rotateZ(90deg)',
+  left: 0
+};
+const rotateH_Rev = {
+  transform: 'rotateZ(0deg)',
+  top: '50%'
+};
+const rotateV = {
+  transform: 'rotateZ(90deg)',
+  top: 0
+};
+
+// Wait for DOM content to be loaded:
+$(function() {
+  $('li').on('click', '.inactive', (evt) => {
+    const $currTarg = $(evt.currentTarget),
+          $accordion = $currTarg.next('.accordion'),
+          $lineH = $currTarg.find('.line-h');
+    $('h4.active').trigger('click');
+    $lineH.css(rotateH);
+    setTimeout(() => {
+      $lineH.parent().css({ transform: 'rotateZ(90deg)' });
+      $currTarg.toggleClass('inactive active');
+    }, 250);
+  });
+  
+  $('li').on('click', '.active', (evt) => {
+    const $currTarg = $(evt.currentTarget),
+          $accordion = $currTarg.next('.accordion'),
+          $lineH = $currTarg.find('.line-h');
+    $currTarg.find('.glyph-wrapper').css({ transform: 'rotateZ(-90deg)' });
+    setTimeout(() => {
+      $lineH.css(rotateH_Rev);
+      $currTarg.toggleClass('inactive active');
+    }, 250);
+  });
+  
+  $('.default').trigger('click');
+});
