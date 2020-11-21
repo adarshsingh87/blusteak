3e1223be90908cc64ac42ad5cf739ba1f630738d
Adarsh Singh <adarshmanoj.05.as@gmail.com>
2020-11-21 23:50:20 +0530

    image autoplay carousel added

--- js/app.js
+++ js/app.js
@@ -42,7 +42,7 @@ jQuery(function ($) {
 
 	$win
 		.on('scroll', function () {
-			if ($(this).scrollTop() > winH*0.9) {
+			if ($(this).scrollTop() > winH * 0.7) {
 				$nav.addClass('doch')
 			} else {
 				$nav.removeClass('doch')
@@ -52,88 +52,117 @@ jQuery(function ($) {
 			// If the user resizes the window
 			winH = $(this).height() // you'll need the new height value
 		})
-} )
+})
 
 // ! carousel
 $('.pri-carousel').owlCarousel({
-    loop:false,
-  stagePadding: 15,
-    margin:10,
-    nav:true,
-  navText : ['<span class="uk-margin-small-right uk-icon" uk-icon="icon: chevron-left"></span>','<span class="uk-margin-small-left uk-icon" uk-icon="icon: chevron-right"></span>'],
-  loop: true,
-    responsive:{
-        0:{
-            items:1
-        },
-        640:{
-            items:2
-        },
-      960:{
-            items:3
-        }
-    }
+	stagePadding: 15,
+	margin: 10,
+	nav: true,
+	navText: [
+		'<span class="uk-margin-small-right uk-icon" uk-icon="icon: chevron-left"></span>',
+		'<span class="uk-margin-small-left uk-icon" uk-icon="icon: chevron-right"></span>',
+	],
+	loop: true,
+	responsive: {
+		0: {
+			items: 1,
+		},
+		640: {
+			items: 2,
+		},
+		960: {
+			items: 3,
+		},
+	},
 })
 
 $('.sec-carousel').owlCarousel({
-    loop:false,
-  stagePadding: 15,
-    margin:10,
-    nav:true,
-  navText : ['<span class="uk-margin-small-right uk-icon" uk-icon="icon: chevron-left"></span>','<span class="uk-margin-small-left uk-icon" uk-icon="icon: chevron-right"></span>'],
-  loop: true,
-    responsive:{
-        0:{
-            items:1
-        },
-        640:{
-            items:1
-        },
-      960:{
-            items:1
-        }
-    }
+	stagePadding: 15,
+	margin: 10,
+	nav: true,
+	navText: [
+		'<span class="uk-margin-small-right uk-icon" uk-icon="icon: chevron-left"></span>',
+		'<span class="uk-margin-small-left uk-icon" uk-icon="icon: chevron-right"></span>',
+	],
+	loop: true,
+	responsive: {
+		0: {
+			items: 1,
+		},
+		640: {
+			items: 1,
+		},
+		960: {
+			items: 1,
+		},
+	},
 })
 
 // ! faq accordian
 // CSS animation maps:
 const rotateH = {
-  transform: 'rotateZ(90deg)',
-  left: 0
-};
+	transform: 'rotateZ(90deg)',
+	left: 0,
+}
 const rotateH_Rev = {
-  transform: 'rotateZ(0deg)',
-  top: '50%'
-};
+	transform: 'rotateZ(0deg)',
+	top: '50%',
+}
 const rotateV = {
-  transform: 'rotateZ(90deg)',
-  top: 0
-};
+	transform: 'rotateZ(90deg)',
+	top: 0,
+}
 
 // Wait for DOM content to be loaded:
-$(function() {
-  $('li').on('click', '.inactive', (evt) => {
-    const $currTarg = $(evt.currentTarget),
-          $accordion = $currTarg.next('.accordion'),
-          $lineH = $currTarg.find('.line-h');
-    $('h4.active').trigger('click');
-    $lineH.css(rotateH);
-    setTimeout(() => {
-      $lineH.parent().css({ transform: 'rotateZ(90deg)' });
-      $currTarg.toggleClass('inactive active');
-    }, 250);
-  });
-  
-  $('li').on('click', '.active', (evt) => {
-    const $currTarg = $(evt.currentTarget),
-          $accordion = $currTarg.next('.accordion'),
-          $lineH = $currTarg.find('.line-h');
-    $currTarg.find('.glyph-wrapper').css({ transform: 'rotateZ(-90deg)' });
-    setTimeout(() => {
-      $lineH.css(rotateH_Rev);
-      $currTarg.toggleClass('inactive active');
-    }, 250);
-  });
-  
-  $('.default').trigger('click');
-});
+$(function () {
+	$('li').on('click', '.inactive', (evt) => {
+		const $currTarg = $(evt.currentTarget),
+			$accordion = $currTarg.next('.accordion'),
+			$lineH = $currTarg.find('.line-h')
+		$('h4.active').trigger('click')
+		$lineH.css(rotateH)
+		setTimeout(() => {
+			$lineH.parent().css({ transform: 'rotateZ(90deg)' })
+			$currTarg.toggleClass('inactive active')
+		}, 250)
+	})
+
+	$('li').on('click', '.active', (evt) => {
+		const $currTarg = $(evt.currentTarget),
+			$accordion = $currTarg.next('.accordion'),
+			$lineH = $currTarg.find('.line-h')
+		$currTarg.find('.glyph-wrapper').css({ transform: 'rotateZ(-90deg)' })
+		setTimeout(() => {
+			$lineH.css(rotateH_Rev)
+			$currTarg.toggleClass('inactive active')
+		}, 250)
+	})
+
+	$('.default').trigger('click')
+} )
+
+
+// ! img carousel
+
+$('.img-carousel').owlCarousel({
+	dots: false,
+	stagePadding: 15,
+	margin: 10,
+	nav: false,
+	loop: true,
+	autoplay: true,
+	autoplayTimeout: 1000,
+  autoplayHoverPause: false,
+	responsive: {
+		0: {
+			items: 2,
+		},
+		640: {
+			items: 3,
+		},
+		960: {
+			items: 5,
+		},
+	},
+})
