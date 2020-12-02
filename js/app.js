7e6f76186504489cab5e16ac97e9350de9e36aff
Adarsh Singh <adarshmanoj.05.as@gmail.com>
2020-12-02 23:24:13 +0530

    counter animation on stats

--- js/app.js
+++ js/app.js
@@ -128,3 +128,32 @@ $('.img-carousel').owlCarousel({
 		},
 	},
 })
+
+// ! counter animation
+// $('.count').each(function () {
+//     $(this).prop('Counter',0).animate({
+//         Counter: $(this).text()
+//     }, {
+//         duration: 2000,
+//         easing: 'swing',
+//         step: function (now) {
+//             $(this).text(Math.ceil(now));
+//         }
+//     });
+// });
+$(window).scroll(startCounter);
+function startCounter() {
+    if ($(window).scrollTop() > 2000) {
+        $(window).off("scroll", startCounter);
+        $('.count').each(function () {
+            var $this = $(this);
+            jQuery({ Counter: 0 }).animate({ Counter: $this.text() }, {
+                duration: 2000,
+                easing: 'swing',
+                step: function () {
+                    $this.text(Math.ceil(this.Counter));
+                }
+            });
+        });
+    }
+}
