e7f68db7671dc0ed51325bd7f2f6d94ad2fef944
Adarsh Singh <adarshmanoj.05.as@gmail.com>
2021-01-09 15:41:59 +0530

    blog js made different

--- js/app.js
+++ js/app.js
@@ -1,18 +1,3 @@
-
-// blog card data
-
-document.getElementById('first-blog-card-img').src = '../assets/blogs/blog29-1.webp'
-document.getElementById('first-blog-card-title').innerHTML = '<div>How Small Businesses Can Take Advantage of Instagram Reels?</div>'
-document.getElementById('first-blog-card-title').href = './how-small-businesses-can-take-advantage-of-instagram-reels.html'
-
-document.getElementById('first-blog-card-img2').src = '../assets/blogs/blog1-1.webp'
-document.getElementById('first-blog-card-title2').innerHTML = '<div>Is the organic reach of LinkedIn going down? – Here’s the proof</div>'
-document.getElementById('first-blog-card-title2').href = './is-the-organic-reach-of-linkedin-going-down-here-s-the-proof.html'
-
-document.getElementById('first-blog-card-img3').src = '../assets/blogs/blog2-1.webp'
-document.getElementById('first-blog-card-title3').innerHTML = '<div>4 Tips on How to Use Giphy to Grow Your Brand or Business</div>'
-document.getElementById('first-blog-card-title3').href = './how-use-giphy-to-grow-your-brand-or-business.html'
-
 // ! navbar
 const navSlide = () => {
 	const burger = document.querySelector('.burger')
