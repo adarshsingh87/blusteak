bfdeb40efd1c246af1ada695c76c5c0e23f7ff96
Adarsh Singh <adarshmanoj.05.as@gmail.com>
2021-08-17 01:59:12 +0530

    clients page changed

--- js/app.js
+++ js/app.js
@@ -56,6 +56,27 @@ jQuery(function ($) {
 		})
 })
 
+// ! tab view stuff
+
+function openCity(evt, cityName) {
+	var i, tabcontent, tablinks
+	tabcontent = document.getElementsByClassName('tabcontent')
+	for (i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].style.display = 'none'
+	}
+	tablinks = document.getElementsByClassName('tablinks')
+	for (i = 0; i < tablinks.length; i++) {
+		tablinks[i].className = tablinks[i].className.replace(' tab-active', '')
+	}
+	document.getElementById(cityName).style.display = 'flex'
+	evt.currentTarget.className += ' tab-active'
+}
+
+// Get the element with id="defaultOpen" and click on it
+// document.getElementById("defaultOpen").click();
+const links = document.querySelectorAll('.tablinks')
+links[0].click()
+
 // ! carousel
 $('.pri-carousel').owlCarousel({
 	stagePadding: 15,
